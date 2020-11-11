e7df38dae8951025691d2441ed658e3bdbf0ab70
griffalo28 <->
2020-11-11 13:35:39 +0000

    cards css

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,17 +4,24 @@ import "../Card/card.css";
 export default function Card({ object }) {
   return (
     <div className="card-container">
-      <div>
-        <img className="image" src="anon.png"></img>
-      </div>
-      <div className="card-body">
-        <h5 className="card-title">
-          <b>
-            {object.firstname} {object.lastname}
-          </b>
-        </h5>
-        <h6 className="card-subtitle">{object.role}</h6>
-  <p className="card-text">{object.bio}</p>
+      <div className="card-inner">
+        <div className="card-front">
+          <div>
+            <img className="image" src="anon.png"></img>
+          </div>
+          <div className="card-body">
+            <h5 className="card-title">
+              <b>
+                {object.firstname} {object.lastname}
+              </b>
+            </h5>
+            <h6 className="card-subtitle">{object.role}</h6>
+          </div>
+        </div>
+
+        <div className="card-back">
+          <p className="card-text">{object.bio}</p>
+        </div>
       </div>
     </div>
   );
