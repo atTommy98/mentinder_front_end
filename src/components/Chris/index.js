7f8129f441e68d47b931e9b1d69e368062685725
= <patrickjosephfleming@gmail.com>
2020-11-12 15:59:01 +0000

    close enough

--- src/components/Chris/index.js
+++ src/components/Chris/index.js
@@ -2,20 +2,31 @@ import React, { useState, useEffect } from "react";
 import "./Chris.css";
 
 function Chris() {
-  const [isShrinking, setIsShrinking] = useState(false);
+  const [isShrinking, setIsShrinking] = useState(1);
 
   useEffect(() => {
     setTimeout(() => {
-      setIsShrinking(true);
+      setIsShrinking(2);
     }, 3000);
-  });
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsShrinking(3);
+    }, 5000);
+  }, []);
+
+  function setClassName() {
+    if (isShrinking === 1) {
+      return "first";
+    } else if (isShrinking === 2) {
+      return "second";
+    } else return "third";
+  }
 
   return (
     <div>
-      <img
-        className={isShrinking ? "shrink" : "notShrinking"}
-        src="/chris-hat3.png"
-      />
+      <img className={setClassName()} src="/chris-hat3.png" />
     </div>
   );
 }
