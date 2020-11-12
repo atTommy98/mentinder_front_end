5e9e5e565232660208283d7afbbd9f7ca0b226b5
griffalo28 <->
2020-11-12 12:23:12 +0000

    flair css

--- src/components/Flairs/index.js
+++ src/components/Flairs/index.js
@@ -2,7 +2,19 @@ import React from "react";
 import "./flairs.css";
 
 function Flair({ expertise }) {
-  return <span className="flair">{expertise}</span>;
+  function nameClass(expertise) {
+    if (
+      expertise === "foodanddrink" ||
+      expertise === "sportandfitness" ||
+      expertise === "gaming" ||
+      expertise === "music" ||
+      expertise === "reading" ||
+      expertise === "films"
+    ) {
+      return "hobbies-flair";
+    } else return "professional-flair";
+  }
+  return <span className={nameClass(expertise)}>{expertise}</span>;
 }
 
 export default Flair;
