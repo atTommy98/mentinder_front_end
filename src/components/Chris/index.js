1f9f83ccc2650d764fd48fd31d0938cd788887a9
= <patrickjosephfleming@gmail.com>
2020-11-13 09:22:30 +0000

    finised

--- src/components/Chris/index.js
+++ src/components/Chris/index.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
 import "./Chris.css";
+import useSound from "use-sound";
+import mentors from "./mentors-[AudioTrimmer.com] (1).mp3";
 
 function Chris() {
   const [isShrinking, setIsShrinking] = useState(false);
   const [isFixed, setIsFixed] = useState(false);
 
+  const [mentorsSound] = useSound(mentors);
+
   useEffect(() => {
+    setTimeout(mentorsSound, 1000);
     setTimeout(() => {
       setIsShrinking(true);
     }, 3000);
